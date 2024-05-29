4e4bf499cc877becd5c061397b0391416546e047
Руслан Кремнёв <Doinktheclown12@yandex.ru>
2024-05-29 16:29:34 +0300

    Добавьте файлы проекта.

--- Settings.cs
+++ Settings.cs
@@ -2,14 +2,9 @@
 {
     internal class Settings
     {
-        // Поле для хранения листа
-        private Dictionary<string, int> _settingsList;
 
         // Свойство для доступа к объектам
         public Dictionary<string, int> SETTINGS { get; private set; }
-
-        // Имя теста
-        private string _name = "";
 
         // Свойство дял доступа к имени
         public string Name { get; set; }
@@ -17,9 +12,8 @@
         // конструктор
         public Settings()
         {
-            _settingsList = new Dictionary<string, int>();
-            SETTINGS = _settingsList;
-            _name = Name;
+            SETTINGS = new Dictionary<string, int>();
+
         }
     }
 }
